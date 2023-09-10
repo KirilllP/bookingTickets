576166e4ce2343a8e4cb1e16249206d459b85428
Mykhailo Marchuk <nisha.mar4uck@gmail.com>
2023-09-11 01:20:10 +0300

    Initial commit

---
+++ src/main/java/com/booking/app/TicketBookingWebServiceApplication.java
@@ -0,0 +1,15 @@
+package com.booking.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class TicketBookingWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/booking/app/config/Config.java
@@ -0,0 +1,20 @@
+package com.booking.app.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import java.util.function.Predicate;
+
+@Configuration
+
+public class Config {
+
+
+
+    }
+
+
+
